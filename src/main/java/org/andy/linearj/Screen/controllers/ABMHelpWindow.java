b7f99cf44dccb5e74be9ea2c02533f1fbcec7a38
Andy.808 <165416703+chilldotrelax@users.noreply.github.com>
2026-06-30 13:48:27 -1000

    Added a new help menu for the "Add Element" menu.

---
+++ src/main/java/org/andy/linearj/Screen/controllers/ABMHelpWindow.java
@@ -0,0 +1,11 @@
+package org.andy.linearj.Screen.controllers;
+
+public class ABMHelpWindow extends PopupWindow {
+
+    public ABMHelpWindow(){
+        super("/org/andy/linearj/AddComponentHelpPopupMenu.fxml","Add Element Help");
+    }
+
+
+
+}
